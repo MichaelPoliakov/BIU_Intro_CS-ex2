c628b18db870fccf87b9456478e466af29cd9253
MichaelPoliakov <poliakov@campus.technion.ac.il>
2025-04-17 18:09:38 +0300

    for now

--- ex2.c
+++ ex2.c
@@ -1,28 +1,75 @@
 /******************
-Name:
-ID:
+Name: Michael Poliakov
+ID: 315940791
 Assignment: ex2
 *******************/
 
 #include <stdio.h>
 
 int main() {
-	// Case 1: Draw Happy Face with given symbols for eyes, nose and mouth
-	/* Example:
+        char i = 0;
+        printf("Choose an option:\n");
+        jump_here:
+          printf("\t1. Happy Face\n\t2. Balanced Number\n\t3. Generous Number\n"
+                 "\t4. Circle Of Joy\n\t5. Happy Numbers\n"
+                 "\t6. Festival Of Laughter\n\t7. Exit\n");
+        if(scanf(" %d", &i) < 1) return 0;
+        if(i<1 || i>7){
+          printf("This option is not available, please try again.\n");
+          goto jump_here;
+        }
+        switch (i) {
+        case 7:
+          printf("Thank you for your journey through Numeria!\n");
+          return 0;
+
+        // Case 1: Draw Happy Face with given symbols for eyes, nose and mouth
+        /* Example:
 	* n = 3:
 	* 0   0
 	*   o
 	* \___/
-	*/
+         */
+        case 1:
+          char eye, nose, mouth;
+          int n = 0;
+          printf("Enter symbols for the eyes, nose, and mouth:\n");
+          if(scanf(" %c %c %c", &eye, &nose, &mouth) < 3) return 1;
+          printf("Enter face size:\n");
+          if(scanf(" %d", &n) <  1) return 1;
+          while((n <= 0) || (n % 2 == 0)){
+            printf("The face's size must be an odd and positive number,"
+                   " please try again:\n");
+            if(scanf(" %d", &n) <  1) return 0;
+          }
+          printf("%c%*c\n", eye, n + 1, eye);
+          printf("%*c\n", (n + 1) / 2 + 1, nose);
+          printf("\\");
+          for(int j=0; j < n; j++) printf("%c", mouth);
+          printf("/\n");
+          break;
+
+          // Case 2: determine whether the sum of all digits to the left of the middle digit(s)
+          // and the sum of all digits to the right of the middle digit(s) are equal
+          /* Examples:
+          Balanced: 1533, 450810, 99
+          Not blanced: 1552, 34
+          Please notice: the number has to be bigger than 0.
+          */
+        case 2:
+          printf("Enter a number:\n");
+          int i, tmp;
+          if(scanf(" %d", &i) < 1) return 1;
+          for(int j)
+          printf("This number is balanced and brings harmony!\n");
+
+
+        }
+
+
     
 
-	// Case 2: determine whether the sum of all digits to the left of the middle digit(s)
-	// and the sum of all digits to the right of the middle digit(s) are equal
-	/* Examples:
-	Balanced: 1533, 450810, 99
-	Not blanced: 1552, 34
-	Please notice: the number has to be bigger than 0.
-	*/
+
 
 	// Case 3: determine whether the sum of the proper divisors (od an integer) is greater than the number itself
 	/* Examples:
